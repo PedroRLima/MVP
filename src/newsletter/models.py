8ab560fcd9ae12d075f5b63c94b4ffed8ac5dd45
Pedro Rodrigues Lima <perliima72@gmail.com>
2016-04-16 17:13:05 -0300

    Model Signup added

--- src/newsletter/models.py
+++ src/newsletter/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Signup(models.Model):
+	full_name = models.CharField(max_length=200, blank=False, null=False)
+	email     = models.EmailField(blank=False, null=False)
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+	updated   = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+	def __unicode__(self):
+		return self.email 
