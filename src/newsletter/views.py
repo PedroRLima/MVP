4710cde8d539648d36141ecebd08ecefa32a92ed
Pedro Rodrigues Lima <perliima72@gmail.com>
2016-04-17 02:37:44 -0300

    Email contact has been added

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+
 from random import randint
 
-from .forms import ContactForm, SignupForm
+from .forms import ContactForm, SignupForm 
 
 
 # Create your views here.
@@ -33,9 +36,27 @@ def contact(request):
 	title = 'Contact'
 	form = ContactForm(request.POST or None)
 	if form.is_valid():
-		name = form.cleaned_data.get('full_name')
-		if not name:
-			name = "User" + str(randint(1,1000)
+		form_name = form.cleaned_data.get('full_name')
+
+		if not form_name:
+			form_name = "User" + str(randint(1,1000))
+
+		form_email   = form.cleaned_data.get('email')
+		form_message = form.cleaned_data.get('message')
+
+		subject = 'Site contact form'
+		from_email = settings.EMAIL_HOST_USER
+		to_email = [from_email]
+		contact_message = '%s: %s via %s' % (form_name, form_message, form_email) 
+
+
+		send_mail(
+			subject, 
+			contact_message, 
+			from_email, 
+			to_email, 
+			fail_silently=False
+			)
 
 			
 
--- src/tryDjango18/settings.py
+++ src/tryDjango18/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#Email settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'perliima72@gmail.com'
+EMAIL_HOST_PASSWORD = 'mlktoczilajpwnov'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Application definition
 
